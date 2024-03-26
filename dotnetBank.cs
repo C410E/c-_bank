048d2de23cd6907c76956699403676d4d037ce3d
caioe <caioevandro0@gmail.com>
2024-03-26 14:15:45 -0300

    starting console project

---
+++ dotnetBank.cs
@@ -0,0 +1,22 @@
+﻿namespace dotnetBank;
+
+public class dotnetBank 
+{
+    public bool logged;
+    public int loggedUser;
+    public int[,] Bank;
+
+    private int maxAccounts = 50;
+    public int registredAccounts;
+    public dotnetBank()
+    {
+        Bank = new int[maxAccounts, 4];
+        loggedUser = -99;
+        logged = false;
+        registredAccounts = 0;
+    }
+    public void RegisterAccount(int number, int agency, int pass)
+    {
+
+    }
+}
