68437923182e83946b3cbb1c99083b08561a22ad
caioe <caioevandro0@gmail.com>
2024-03-26 16:23:13 -0300

    adding new features to get and set

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@ class MoviesPrincipal
 {
     public static void Main(string[] args)
     {
-        Movie movieA = new Movie("Matrix", "Ação");
+       try 
+       {
+        Movie movieA = new Movie("Matrix", "Ficção científica");
         Movie movieB = new Movie("Senhor dos Aneis", "Fantasia");
+
+        movieA.Category = "Fantasia";
 
         Console.WriteLine(movieA.Title);
         Console.WriteLine(movieA.Category);
         Console.WriteLine(movieB.Title);
         Console.WriteLine(movieB.Category);
+       }
+       catch (Exception ex)
+       {
+            Console.WriteLine("Erro: " + ex.Message);
+            throw;
+       }
     }
 }
